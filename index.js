4a8a54fc202fb3ad59a67d9fede8d3e0bb996147
enkw <iankwolfe@gmail.com>
2024-04-03 18:50:25 -0400

    Added questions for adding elements to the db

--- index.js
+++ index.js
@@ -27,11 +27,46 @@ const menu = [
     }
 ];
 
-const employeeQ = [];
+const employeeQ = [
+    {
+        type: 'input',
+        name: 'firstname',
+        message: 'Please enter the Employees first name:'
+    },
+    {
+        type: 'input',
+        name: 'lastname',
+        message: 'Please enter the Employees last name:'
+    },
+    {
+        type: 'list',
+        name: 'role',
+        message: 'Please select the Employees role:',
+        choices: []
+    }
+];
 
-const roleQ = [];
+const roleQ = [
+    {
+        type: 'list',
+        name: 'departmentlist',
+        message: 'Please select the department for this new role:',
+        choices: []
+    },
+    {
+        type: 'input',
+        name: 'rolename',
+        message: 'Please enter the name of this new role:'
+    }
+];
 
-const departQ = [];
+const departQ = [
+    {
+        type: 'input',
+        name: 'departmentname',
+        message: 'Please enter the name of the new department:'
+    }
+];
 
 // function init() {
 //     inquirer.prompt(menu).then()
